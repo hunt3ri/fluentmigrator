91ced53085869857282a90c6675d3c2732d45dd2
Iain Hunter <iain.hunter@huzutech.com>
2012-07-26 17:15:41 +0100

    Working test runner

---
+++ src/Huzutech.Tests/TestRunner.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentMigrator.Runner;
+using FluentMigrator.Runner.Announcers;
+using FluentMigrator.Runner.Initialization;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace Huzutech.Tests
+{
+	[TestFixture]
+	public class TestRunner 
+	{
+		[Test]
+		public void RunMigrations()
+		{
+			IAnnouncer announcer = new ConsoleAnnouncer
+			{
+				ShowElapsedTime = false,
+				ShowSql = false
+			};
+
+			var runnerContext = new RunnerContext(announcer)
+			{
+				Database = "sqlserver2008",
+				Connection = "",
+				Target = @"C:\dev\work\fluentmigrator\src\Huzutech.Migrations\bin\Debug\Huzutech.Migrations.dll",
+				Version = 1,
+			};
+
+			new TaskExecutor(runnerContext).Execute();
+		}
+	}
+}
